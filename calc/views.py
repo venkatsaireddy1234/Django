65f3d734b56b002a07f0de57de6669c7b5e31636
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-01-09 19:23:18 +0530

    Add addition functionality and update templates for number input

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'concept': 'DTL'} )
+
+def add(request):
+    val1 = int(request.POST['number1'])
+    val2 = int(request.POST['number2'])
+    res = val1 +val2
+    return render(request, 'result.html', {'res':res})
